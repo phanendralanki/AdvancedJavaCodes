a9d1e320703256bcbf35467a90039a4e548d3431
phanendralanki <phanendralg12@gmail.com>
2023-04-18 23:14:35 +0530

    added the code for getters and setters

---
+++ UseOFGettersAndSetters/src/com/code/Main.java
@@ -0,0 +1,14 @@
+package com.code;
+
+public class Main {
+	public static void main(String[] args) {
+		Student st = new Student();
+		//System.out.println(st.name);
+		//System.out.println(st.rollNumber);
+		
+		st.setRollNumber("237");
+		st.setName("phani");
+		
+		st.display();
+	}
+}
--- UseOFGettersAndSetters/src/com/code/Student.java
+++ UseOFGettersAndSetters/src/com/code/Student.java
@@ -1,8 +1,28 @@
 package com.code;
 
 public class Student {
-	String name;
-	String rollNumber;
+	private String name;
+	private String rollNumber;
 	
+	public void display() {
+		System.out.println(this.name);
+		System.out.println(this.rollNumber);
+	}
 	
+	//getter
+	public String getName() {
+		return this.name;
+	}
+	//setter
+	public void setName(String name) {
+		this.name = name; //parameter name
+	}
+	
+	public String getRollNumber() {
+		return this.rollNumber;
+	}
+	//setter
+	public void setRollNumber(String rollNumber) {
+		this.rollNumber = rollNumber; //parameter name
+	}
 }
