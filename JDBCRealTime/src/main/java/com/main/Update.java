394f1c09bb2681c76e9d4c3497cf2f73bd6bfc40
phanendralanki <phanendralg12@gmail.com>
2023-04-20 23:59:01 +0530

    update.java is created to write the code that updates the student details in the db

---
+++ JDBCRealTime/src/main/java/com/main/Update.java
@@ -0,0 +1,15 @@
+package com.main;
+import java.util.*;
+
+import com.bean.Student;
+public class Update {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Student st = new Student();
+		
+		
+		
+		sc.close();
+	}
+}
