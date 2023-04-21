fe8c045a3661300af093b1984df2d7bdaeab2d25
phanendralanki <phanendralg12@gmail.com>
2023-04-21 22:55:46 +0530

    added changes to the update code

--- JDBCRealTime/src/main/java/com/main/Update.java
+++ JDBCRealTime/src/main/java/com/main/Update.java
@@ -2,14 +2,27 @@ package com.main;
 import java.util.*;
 
 import com.bean.Student;
+import com.connection.DBConnect;
+import com.dao.StudentDao;
 public class Update {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		Student st = new Student();
 		
+		st.setId(1);
+		st.setName("Gundu chandra pavan");
+		st.setMobile("8008128131");
+		st.setAge("25");
 		
+		StudentDao dao = new StudentDao(DBConnect.getConnection());
 		
+		boolean f = dao.editStudent(st);
+		if(f) {
+			System.out.println("student details updated");
+		}else {
+			System.out.println("sorry");
+		}
 		sc.close();
 	}
 }
