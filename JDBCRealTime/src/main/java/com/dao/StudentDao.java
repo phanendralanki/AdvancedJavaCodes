4ae0e22da3130e5b72a895cad1b094d3f7e628db
phanendralanki <phanendralg12@gmail.com>
2023-04-22 23:30:13 +0530

    Added changes in the studentdao for clean code

--- JDBCRealTime/src/main/java/com/dao/StudentDao.java
+++ JDBCRealTime/src/main/java/com/dao/StudentDao.java
@@ -1,8 +1,11 @@
 package com.dao;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.bean.Student;
+import com.connection.DBConnect;
 //import com.connection.DBConnect;
 
 public class StudentDao {
@@ -67,5 +70,68 @@ public class StudentDao {
 		return f;
 	}
 	
+	
+	public boolean deleteData(int id) {
+		boolean f = false;
+			
+		try {
+			PreparedStatement ps = conn.prepareStatement("delete from students where id=?");
+			StudentDao dao = new StudentDao(DBConnect.getConnection());
+			ps.setInt(1, id);
+			int i = ps.executeUpdate();
+			if(i>=1) {
+				f = true;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+		return f;
+	}
 
+	
+	public Student viewData(int id) {
+		Student st = null;
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement("select *from students where id=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				st = new Student();
+				st.setId(rs.getInt(1));
+				st.setName(rs.getString(2));
+				st.setMobile(rs.getString(3));
+				st.setAge(rs.getString(4));
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return st;
+		
+	}
+	
+	public List<Student> getAllData(){
+		List<Student> list = new ArrayList();
+		Student st = null;
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement("select *from students");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				st = new Student();
+				st.setId(rs.getInt(1));
+				st.setName(rs.getString(2));
+				st.setMobile(rs.getString(3));
+				st.setAge(rs.getString(4));
+				list.add(st);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 }
